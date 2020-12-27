0ddf2f2d71cfcdfd894ba81caf95c66d2d2d870a
Savvas Mantzouranidis <mohito6@hotmail.com>
2020-12-27 15:30:47 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -8,7 +8,7 @@ $pass=$DB_PASS;
 
 
 if(gethostname()=='users.iee.ihu.gr') {
-    $mysqli = new mysqli($host, $user, $pass, $db, null, '/home/staff/asidirop/mysql/run/mysql.sock');
+    $mysqli = new mysqli($host, $user, $pass, $db, null, '/home/student/it/2017/it174883/mysql/run/mysql.sock');
 } else {
         $mysqli = new mysqli($host, $user, $pass, $db);
 }
